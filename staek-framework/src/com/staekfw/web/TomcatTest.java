5a33e51822df7b6a50529c126dc3f209a2c11c39
seongtaekkim <chxortnl@gmail.com>
2023-08-03 19:53:12 +0900

    create project

---
+++ staek-framework/src/com/staekfw/web/TomcatTest.java
@@ -0,0 +1,44 @@
+package com.staekfw.web;
+
+import org.apache.catalina.Context;
+import org.apache.catalina.startup.Tomcat;
+
+import javax.servlet.http.HttpServlet;
+import java.io.File;
+
+/**
+ * @author seongtaek
+ *
+ */
+public class TomcatTest {
+
+	/**
+	 * @param args
+	 * @throws Exception
+	 */
+	public static void main(String[] args) throws Exception {
+
+		String root = new File(".").getAbsolutePath();
+
+		String baseDir = root + File.separatorChar + "temp";
+
+		int webPort = 18080;
+
+		Tomcat tomcat = new Tomcat();
+		tomcat.setPort(webPort);
+
+		tomcat.setBaseDir(baseDir);
+		tomcat.setPort(webPort);
+
+		Context context = tomcat.addContext("/", baseDir);
+
+		HttpServlet board = new ServletTest();
+
+		tomcat.addServlet("/", "test", board);
+		context.addServletMappingDecoded("/test", "test");
+
+		tomcat.start();
+		tomcat.getServer().await();
+
+	}
+}
