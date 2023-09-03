c7c0bc4dde45773d6af2b69b0b485ababfd7070f
seongtaekkim <chxortnl@gmail.com>
2023-09-03 15:31:00 +0900

    Feat: MOD throw catch to try-chatch

--- staek-jdbc/src/main/java/me/staek/Main.java
+++ staek-jdbc/src/main/java/me/staek/Main.java
@@ -2,24 +2,33 @@ package me.staek;
 
 import java.io.File;
 import java.sql.*;
+import java.util.Random;
 
 public class Main {
 
-
-    public static void main(String[] args) throws ClassNotFoundException, SQLException {
+    public static void main(String[] args) {
 
         String className = "org.sqlite.JDBC";
 
         // org.sqlite.JDBC static 생성자 호출
-        Class<?> aClass = Class.forName(className);
+        Class<?> aClass = null;
+        try {
+            aClass = Class.forName(className);
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
         System.out.println(aClass.getName());
 
         File file = new File(".");
         String url = "jdbc:sqlite:" + file.getAbsolutePath() + "/data/dev.db";
 
 
-        Connection conn = DriverManager.getConnection(url);
-
+        Connection conn = null;
+        try {
+            conn = DriverManager.getConnection(url);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
 
         String sql = "CREATE TABLE IF NOT EXISTS user (\n"
                 + "	id integer PRIMARY KEY,\n"
@@ -34,17 +43,25 @@ public class Main {
             System.out.println(e.getMessage());
         }
 
+        
+        PreparedStatement ps = null;
+        try {
+            ps = conn.prepareStatement("insert into user(id,name) values(?,?)");
+            User user = new User();
+            Random r = new Random();
 
-        PreparedStatement ps = conn.prepareStatement("insert into user(id,name) values(?,?)");
-        User user = new User();
-        user.setId("2");
-        user.setName("kim");
-        ps.setString(1, user.getId());
-        ps.setString(2, user.getName());
+            user.setId("2");
+            user.setName("kim");
+            ps.setString(1, user.getId());
+            ps.setString(2, user.getName());
 
-        ps.executeUpdate();
+            ps.executeUpdate();
 
-        ps.close();
-        conn.close();
+            ps.close();
+            conn.close();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
     }
 }
