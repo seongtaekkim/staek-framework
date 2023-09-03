074b1a5656dd0c51d34af83a4f80acb4e7b0d7ca
seongtaekkim <chxortnl@gmail.com>
2023-09-03 10:52:06 +0900

    Feat: ADD connect sqlite db

--- staek-jdbc/src/main/java/me/staek/Main.java
+++ staek-jdbc/src/main/java/me/staek/Main.java
@@ -1,7 +1,23 @@
 package me.staek;
 
+import java.io.File;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("hello jdbc project");
+    public static void main(String[] args) throws ClassNotFoundException, SQLException {
+
+        String className = "org.sqlite.JDBC";
+
+        // org.sqlite.JDBC static 생성자 호출
+        Class<?> aClass = Class.forName(className);
+        System.out.println(aClass.getName());
+
+        File file = new File(".");
+        String url = "jdbc:sqlite:" + file.getAbsolutePath() + "/data/dev.db";
+
+
+        Connection conn = DriverManager.getConnection(url);
     }
 }
