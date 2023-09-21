0613f3536154d88b92f8db95232629027cb83a30
seongtaekkim <chxortnl@gmail.com>
2023-09-21 21:08:12 +0900

    Refact: Mod DaoFactory to DI #37
    - DaoFactory @deprecate 처리
    - ServletContextListener 구현체에서 Dao 정보를 스캔하여 인스턴스등록 된 정보를
      ServletContext에 저장하고, 사용하도록 로직변경.

--- staekframework/src/main/java/com/staekframework/business/DaoFactory.java
+++ staekframework/src/main/java/com/staekframework/business/DaoFactory.java
@@ -3,6 +3,9 @@ package com.staekframework.business;
 import com.staekframework.jdbc.Datasource;
 import com.staekframework.jdbc.JDBCConnection;
 
+/**
+ * @deprecated staek-di 으로 인스턴스 주입함.
+ */
 public class DaoFactory {
 
     public UserDao newUserDao() {
--- staekframework/src/main/java/com/staekframework/web/listener/ContextLoaderListener.java
+++ staekframework/src/main/java/com/staekframework/web/listener/ContextLoaderListener.java
@@ -1,19 +1,34 @@
 package com.staekframework.web.listener;
 
 
-import com.staekframework.business.DaoFactory;
+import com.staekframework.di.ScanAndNewInstance;
+import com.staekframework.jdbc.Datasource;
+import com.staekframework.jdbc.JDBCConnection;
 
 import javax.servlet.ServletContext;
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
 import javax.servlet.annotation.WebListener;
+import java.util.Iterator;
 
 @WebListener
 public class ContextLoaderListener implements ServletContextListener {
     @Override
     public void contextInitialized(ServletContextEvent event) {
         ServletContext context = event.getServletContext();
-        context.setAttribute("userDao",new DaoFactory().newUserDao());
+
+        /**
+         * Connection 정보는
+         * 설정정보를 읽어오는 로직에서 생성 및 인스턴스등록할 수 있도록 변경 필요.
+         */
+        Datasource ds = new JDBCConnection();
+        ScanAndNewInstance.putInstance(ds);
+        ScanAndNewInstance.scanAndCreateInstance();
+        Iterator<Object> it = ScanAndNewInstance.getObjectMap().values().iterator();
+        while (it.hasNext()) {
+            Object o = it.next();
+            context.setAttribute(o.getClass().getSimpleName(), o);
+        }
     }
 
     @Override
