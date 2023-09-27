ff4c7c8fb059161597e07f1b9ad9b19273bef5f4
seongtaekkim <chxortnl@gmail.com>
2023-09-27 18:03:19 +0900

    Test: MOD JdbcTemplate를 이용해서 users 테이블 조회 테스트 #46

--- staekframework-practice/src/main/java/com/staekframework/main/Application.java
+++ staekframework-practice/src/main/java/com/staekframework/main/Application.java
@@ -1,35 +1,41 @@
 package com.staekframework.main;
 
+import com.staekframework.businiss.User;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 
 import javax.sql.DataSource;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.List;
 
 @SpringBootApplication
 public class Application {
+
+
     public static void main(String[] args) {
         ConfigurableApplicationContext run = SpringApplication.run(Application.class, args);
         System.out.println(run.getBean(DataSource.class));
 
         DataSource dataSource = run.getBean(DataSource.class);
-
-
-        try {
-            Connection conn = dataSource.getConnection();
-            PreparedStatement ps = conn.prepareStatement("select id, name from users;");
-            ResultSet rs = ps.executeQuery();
-            String city = null;
-            while (rs.next()) {
-                System.out.println(rs.getString(2));;
+        JdbcTemplate jdbcTemplate = new JdbcTemplate(dataSource);
+        List<User> list = jdbcTemplate.query("select * from users", new RowMapper<User>() {
+            @Override
+            public User mapRow(ResultSet rs, int rowNum) throws SQLException {
+                return new User(rs.getString("id")
+                        , rs.getString("name")
+                        , rs.getString("password")
+                        , rs.getString("price"));
             }
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        }
+        });
+
+        list.stream().forEach(System.out::println);
 
     }
 }
