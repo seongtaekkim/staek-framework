7842637bc2e9aecb5e6be5b6f55f5c09a0f6895d
seongtaekkim <chxortnl@gmail.com>
2023-10-04 22:15:15 +0900

    Test: test framework를 이용한 테스트 코드 작성

--- staekframework/src/test/java/com/staekframework/business/UserServiceTest.java
+++ staekframework/src/test/java/com/staekframework/business/UserServiceTest.java
@@ -3,6 +3,7 @@ package com.staekframework.business;
 import com.staekframework.jdbc.Datasource;
 import com.staekframework.jdbc.JDBCConnection;
 import com.staekframework.tx.DefaultTxManager;
+import com.staekframework.tx.TxFactoryProxy;
 import com.staekframework.tx.TxHandler;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Assertions;
@@ -57,7 +58,8 @@ class UserServiceTest {
     void 무결성검사_유저추가() {
         User user = new User("1", "kim", "1111", "10");
         UserService service = new UserServiceImpl(userDao);
-        TxHandler txHandler = new TxHandler(service);
+        TxHandler txHandler = new TxHandler();
+        txHandler.setTarget(service);
         txHandler.setTxManager(new DefaultTxManager(JDBCConnection.conn));
 
         UserService o = (UserService) Proxy.newProxyInstance(getClass().getClassLoader()
@@ -128,7 +130,9 @@ class UserServiceTest {
         users.stream().forEach(System.out::println);
 
         UserService service = new UserServiceImpl(userDao);
-        TxHandler txHandler = new TxHandler(service);
+        TxHandler txHandler = new TxHandler();
+        txHandler.setTarget(service);
+
         txHandler.setTxManager(new DefaultTxManager(JDBCConnection.conn));
         /**
          * 다이나믹 프록시 생성 코드
@@ -137,6 +141,42 @@ class UserServiceTest {
         UserService o = (UserService) Proxy.newProxyInstance(getClass().getClassLoader()
                 , new Class[]{UserService.class}
                 , txHandler);
+        try {
+            o.callwithdrawal_program();
+        } catch (Exception e) {
+            System.out.println("fail");
+        }
+
+        List<User> users2 = userDao.selectAll();
+        users2.stream().forEach(System.out::println);
+        assertThat(users).isEqualTo(users2);
+    }
+
+    @Test
+    void 무결성오류_검사_FactoryProxy() throws Exception {
+        List<User> list = new ArrayList<>();
+        list.add(new User("1", "kim", "1111", "50000"));
+        list.add(new User("2", "kim2", "1111", "45000"));
+        list.add(new User("3", "kim3", "1111", "35000"));
+        list.add(new User("4", "kim4", "1111", "20000"));
+        list.add(new User("5", "kim5", "1111", "10000"));
+        for (User user : list) {
+            userDao.insert(user);
+        }
+        List<User> users = userDao.selectAll();
+        users.stream().forEach(System.out::println);
+
+        UserService service = new UserServiceImpl(userDao);
+        TxFactoryProxy txFactoryProxy = new TxFactoryProxy();
+        txFactoryProxy.setServiceInterface(UserService.class);
+        txFactoryProxy.setTarget(service);
+        txFactoryProxy.setTxManager(new DefaultTxManager(JDBCConnection.conn));
+
+        /**
+         * 다이나믹 프록시 생성 코드
+         * 동적으로 생성되는 다이내믹 프록시 클래스의 로딩에 사용할 클래스 로더, 구현할 인터페이스, 타겟 클래스를 담은 InvocationHandler 구현체
+         */
+        UserService o = (UserService) txFactoryProxy.getObject();
         try {
             o.callwithdrawal_program();
         } catch (Exception e) {
