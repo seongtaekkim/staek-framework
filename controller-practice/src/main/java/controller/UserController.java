156e30f3c8de1b9e7316275548f4600b1d359183
seongtaekkim <chxortnl@gmail.com>
2023-12-13 18:04:51 +0900

    Feat: ADD user controller 생성

---
+++ controller-practice/src/main/java/controller/UserController.java
@@ -0,0 +1,12 @@
+package controller;
+
+public class UserController {
+
+    public void login() {
+        System.out.println("login controller");
+    }
+
+    public void register() {
+        System.out.println("register controller");
+    }
+}
