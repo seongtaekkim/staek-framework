891c8a77fab907406d7400ad1d4eb523d1290151
seongtaekkim <chxortnl@gmail.com>
2023-12-18 20:19:29 +0900

    Feat: ADD url 요청에 parameter가 입력될 때, controller dto 인스턴스 생성후 필드를 매핑하는 기능추가

--- controller-practice/src/main/java/filter/Dispatcher.java
+++ controller-practice/src/main/java/filter/Dispatcher.java
@@ -7,8 +7,12 @@ import javax.servlet.*;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.lang.annotation.Target;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
+import java.lang.reflect.Parameter;
+import java.util.Enumeration;
 
 public class Dispatcher implements Filter {
     @Override
@@ -31,19 +35,63 @@ public class Dispatcher implements Filter {
         UserController userController = new UserController();
         Method[] method = userController.getClass().getDeclaredMethods();
 
+        boolean isFound = false;
+//        System.out.println("request.getParameter  " + request.getParameter("username"));
         for (Method m : method) {
             RequestMapping annotation = m.getDeclaredAnnotation(RequestMapping.class);
+
             if (annotation.value().equals(endPoint)) {
                 try {
-                    String path = (String) m.invoke(userController);
+
+                    Parameter[] param = m.getParameters();
+                    String path = null;
+                    if (param.length != 0) {
+                            Object dto = param[0].getType().getConstructor().newInstance();
+                            setDate(dto, request);
+                            path = (String) m.invoke(userController, dto);
+                        } else {
+                            path = (String) m.invoke(userController);
+                        }
                     RequestDispatcher rd = request.getRequestDispatcher(path);
                     rd.forward(request, response);
-                } catch (IllegalAccessException | InvocationTargetException e) {
+                } catch (IllegalAccessException | InvocationTargetException | InstantiationException | NoSuchMethodException e) {
                     throw new RuntimeException(e);
                 }
+                isFound = true;
                 break ;
             }
         }
+
+         if (!isFound) {
+            response.setContentType("text/html; charset=utf-8");
+            PrintWriter pw = response.getWriter();
+            pw.println("잘못된 주소");
+            pw.flush();
+        }
+    }
+
+    private <T> void setDate(T dto, HttpServletRequest request) throws InvocationTargetException, IllegalAccessException {
+        Enumeration<String> params = request.getParameterNames();
+        while (params.hasMoreElements()) {
+            String param = params.nextElement();
+            String methodName = paramToMethodName(param);
+            System.out.println(methodName);
+            Method[] methods = dto.getClass().getDeclaredMethods();
+            for (Method m : methods) {
+                if (m.getName().equals(methodName)) {
+                    /**
+                     * 타입 체크 로직 필요함
+                     */
+                    m.invoke(dto, request.getParameter(param));
+                }
+            }
+        }
+
+    }
+
+    private String paramToMethodName(String param) {
+        String upper  = param.substring(0,1).toUpperCase();
+        return "set" + upper + param.substring(1);
     }
 
     @Override
