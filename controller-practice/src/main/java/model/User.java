d175b058d89d404dc4273bd4884143ca72d5f3e2
seongtaekkim <chxortnl@gmail.com>
2023-12-18 20:16:48 +0900

    Feat: ADD model user

---
+++ controller-practice/src/main/java/model/User.java
@@ -0,0 +1,50 @@
+package model;
+
+public class User {
+    private int id;
+    private String username;
+    private String password;
+    private String email;
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", username='" + username + '\'' +
+                ", password='" + password + '\'' +
+                ", email='" + email + '\'' +
+                '}';
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
