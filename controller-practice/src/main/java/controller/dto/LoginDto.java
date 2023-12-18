fcf980d1ed387c3a960494e09736453ddc367001
seongtaekkim <chxortnl@gmail.com>
2023-12-18 20:17:28 +0900

    Feat: ADD dto login, register

---
+++ controller-practice/src/main/java/controller/dto/LoginDto.java
@@ -0,0 +1,30 @@
+package controller.dto;
+
+public class LoginDto {
+    private String username;
+    private String password;
+
+    @Override
+    public String toString() {
+        return "LoginDto{" +
+                "username='" + username + '\'' +
+                ", password='" + password + '\'' +
+                '}';
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
