a7b424908398c0bdd9728bce1b981ccff4584770
seongtaekkim <chxortnl@gmail.com>
2023-12-18 20:18:08 +0900

    Feat: MOD login, regist method에 dto 인자를 추가

--- controller-practice/src/main/java/controller/UserController.java
+++ controller-practice/src/main/java/controller/UserController.java
@@ -1,19 +1,23 @@
 package controller;
 
 import annotation.RequestMapping;
+import controller.dto.LoginDto;
+import controller.dto.RegisterDto;
 
 public class UserController {
 
 
     @RequestMapping("/login")
-    public String login() {
+    public String login(LoginDto dto) {
         System.out.println("login controller");
+        System.out.println(dto);
         return "/index.jsp";
     }
 
     @RequestMapping("/register")
-    public String register() {
+    public String register(RegisterDto dto) {
         System.out.println("register controller");
+        System.out.println(dto);
         return "/";
     }
 }
