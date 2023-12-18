a31bdbb18c55dda712d6ef6559b0a5e75a9d14c0
seongtaekkim <chxortnl@gmail.com>
2023-12-18 18:36:59 +0900

    Refactor: request mapping annotation 적용

--- controller-practice/src/main/java/controller/UserController.java
+++ controller-practice/src/main/java/controller/UserController.java
@@ -1,12 +1,19 @@
 package controller;
 
+import annotation.RequestMapping;
+
 public class UserController {
 
-    public void login() {
+
+    @RequestMapping("/login")
+    public String login() {
         System.out.println("login controller");
+        return "/index.jsp";
     }
 
-    public void register() {
+    @RequestMapping("/register")
+    public String register() {
         System.out.println("register controller");
+        return "/";
     }
 }
